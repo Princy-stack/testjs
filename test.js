a5cdc016154fc5e1806cff97f41aedccc2edc7fa
Ghosti <randrianalisonprincy@gmail.com>
2022-08-19 07:55:11 +0300

    testjs

---
+++ test.js
@@ -0,0 +1,23 @@
+// var d = [2, 6, 3, 4, 3, 10];
+// var f = [2];
+
+// function multiplication (d, f){
+//     for (let pointer = 0; pointer < multiplication.length; pointer++);
+//     console.log("multiplication");
+//      return d*f ;
+// }
+
+var tab = ["2", "6", "3", "4", "3", "10"];
+    console.log (pointer);
+    console.log(tab [pointer]);
+    for (let pointer = 0; pointer < object.length; pointer ++) {
+        tab.shift();
+    }
+
+// var tableau = ["2, 6, 3, 4, 3, 10"];
+
+// for (let pointer = tableau.length -1; pointer >= 0; pointer --) {
+//     console.log (pointer);
+//     console.log (tableau[pointer]);
+// }
+
